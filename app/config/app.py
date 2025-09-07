c40b803da653d06fef52bca3453030008491dd50
Carlos Sanchez <carlos.sanchez@heru-app.com>
2025-09-07 13:58:23 -0600

    feat: CSRS disabled

--- app/config/app.py
+++ app/config/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from app.config.db import init_db, close_db
 from app.routes import hanzi, file, category, sentence
@@ -17,6 +18,14 @@ def create_app() -> FastAPI:
         description="API for learning Mandarin Hanzis",
         version="1.0.0",
         lifespan=lifespan,
+    )
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
     )
 
     app.include_router(hanzi.router, prefix="/api/hanzi", tags=["hanzi"])
